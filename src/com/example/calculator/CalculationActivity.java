eea591679153b077ccf8ff5295071c0ba28110fe
S. M. Kamaruzzaman <kamaru.zuel@gmail.com>
2015-01-12 13:30:07 +0600

    first commit

---
+++ src/com/example/calculator/CalculationActivity.java
@@ -0,0 +1,15 @@
+package com.example.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Zuel on 09-01-2015.
+ */
+public class CalculationActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculation);
+    }
+}
